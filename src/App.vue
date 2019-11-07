1b40939c8027bb659669cc2d8241c52a201b5a7e
Bitman <pawelrozumek@gmail.com>
2019-11-07 17:31:53 +0100

    start

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <app-server-status></app-server-status>
+    <hr />
+    <div class="row">
+      <app-servers></app-servers>
+      <app-info></app-info>
+    </div>
+    <hr />
+    <app-footer></app-footer>
+  </div>
+</template>sdfsd
+
+<script>
+import AppServers from "./Servers.vue";
+import AppServerStatus from "./ServerStatus.vue";
+import AppInfo from "./Info.vue";
+import AppFooter from "./Foot.vue";
+
+export default {
+  components: {
+    AppServers,
+    AppServerStatus,
+    AppInfo,
+    AppFooter
+  }
+};
+</script>
+
+<style>
+</style>
